e04369a5d9e19028b8297e574cb7dd7a74f61cc2
cxj <cxj@cxjdeMacBook-Pro.local>
2016-08-07 23:56:45 +0800

    first

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -8,11 +8,11 @@ class IndexController extends Controller {
     }
     public function page(){
     	$id = $_GET['id'];
-    	$this->display('index/page/'.$id);
+    	$this->display('Index/page/'.$id);
     }
     public function news(){
     	$id = $_GET['id'];
-    	$this->display('index/news/'.$id);
+    	$this->display('Index/news/'.$id);
     }
     public function news_view(){
     	$id = $_GET['id'];
@@ -20,7 +20,7 @@ class IndexController extends Controller {
     }
     public function teamnews(){
         $id = $_GET['id'];
-        $this->display('index/teamnews/'.$id);
+        $this->display('Index/teamnews/'.$id);
     }
     public function teamnews_view(){
         $id = $_GET['id'];
@@ -28,7 +28,7 @@ class IndexController extends Controller {
     }
     public function xspage(){
         $id = $_GET['id'];
-        $this->display('index/xspage/'.$id);
+        $this->display('Index/xspage/'.$id);
     }
     public function event_view(){
         $id = $_GET['id'];
@@ -36,7 +36,7 @@ class IndexController extends Controller {
     }
     public function enews_view(){
         $id = $_GET['id'];
-        $this->display('index/event_view/enews_view');
+        $this->display('Index/event_view/enews_view');
     }
     public function edupage(){
         $id = $_GET['id'];
@@ -44,11 +44,11 @@ class IndexController extends Controller {
     }
     public function edupage2(){
         $id = $_GET['id'];
-        $this->display('index/edupage2/'.$id);
+        $this->display('Index/edupage2/'.$id);
     }
     public function teamwork(){
         $id = $_GET['id'];
-        $this->display('index/teamwork/'.$id);
+        $this->display('Index/teamwork/'.$id);
     }
 }
 
